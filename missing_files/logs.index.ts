394521a7e43437fa1b6bdff90d6d6d812ec19082
kpurna <kpurna543@gmail.com>
2026-04-14 10:52:02 +0100

    Delete missing_files directory

--- missing_files/logs.index.ts
+++
@@ -1,7 +0,0 @@
-import { createRouter } from "../../lib/create-app.js";
-import * as handlers from "./logs.js";
-import * as routes from "./logs.routes.js";
-
-const router = createRouter().openapi(routes.getLogs, handlers.getLogs);
-
-export default router;
